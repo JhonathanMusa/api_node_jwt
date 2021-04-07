6ff1e28e69649e7ef5d0c8fb10a9b09d732c53d1
jhonathanmusa <jhonathansamu91@gmail.com>
2021-04-06 20:27:38 -0500

    adding some styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,10 @@ import Login from "./components/Login";
 function App() {
   return (
     <div className="App">
+      <div className="nav">
+        <a>Home</a>
+        <a className="login">Login</a>
+      </div>
       <Login />
       <AddUsers />
     </div>
