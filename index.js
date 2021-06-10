92588b6c61f00225a0f63770bdcb48ee7a99bfa9
jhonathanmusa <jhonathansamu91@gmail.com>
2021-06-09 22:17:22 -0500

    updated application and styles

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const app = express();
+const cors = require("cors");
+
 const authRoutes = require("./routes/auth");
 require("dotenv").config();
 const dashboardRoutes = require("./routes/dashboard");
 const verifyToken = require("./routes/validate-token");
-const cors = require("cors");
+
+const PORT = process.env.PORT || 5000;
+const app = express();
 
 // Capture Body
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 // conexion a base de datos
-const uri = `mongodb://localhost:27017/${process.env.DBNAME}`;
+const uri = process.env.MONGOOSE_URI + process.env.DBNAME;
 const option = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -26,8 +29,10 @@ mongoose
 
 // import routes
 app.use("/api/user", authRoutes);
+
 // route middleware
 app.use("/api/dashboard", verifyToken, dashboardRoutes);
+
 app.get("/", (req, res) => {
   res.json({
     estado: true,
@@ -36,7 +41,6 @@ app.get("/", (req, res) => {
 });
 
 // Iniciar server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, (req, res) => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
